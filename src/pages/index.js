29307f3963ac5d79d124ed7db00de197c7d8679b
Sergey Fedorov <coby22@yandex.ru>
2020-11-05 23:03:21 +0300

    8 sprint webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import FormValidator from './FormValidator.js';
-import Card from './Card.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
-
+import './index.css';
+import FormValidator from '../components/FormValidator.js';
+import Card from '../components/Card.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 const validationConfig = {
     formSelector: '.popup__forms_type_input',
